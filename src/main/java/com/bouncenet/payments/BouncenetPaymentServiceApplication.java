b9893e6bc49700ec0fca1af4876e50d3a5863170
somendra87 <somendraprakash6988@gmail.com>
2026-04-03 11:49:35 +0530

    Initial Commit

---
+++ src/main/java/com/bouncenet/payments/BouncenetPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bouncenet.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouncenetPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BouncenetPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bouncenet/payments/BouncenetPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bouncenet.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BouncenetPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
